a2344e77b3ef8be0f605ef909f3f3c40d5f1498f
Marcy Silverman <marcysilve225@gmail.com>
2019-01-06 21:27:34 -0800

    added different transition to work page

--- src/components/Work.vue
+++ src/components/Work.vue
@@ -2,93 +2,115 @@
   <body>
     <ul>
       <li>
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVCSQRVmXcyfcmW8bd6IBj4fTDpZAYmFIQlDibBOPbxAT0Ha2miw" width="100%" height="100%">
+        <img src="../assets/pasta-css.png" width="100%" height="100%">
         <div class="info">
-          <h3> project </h3>
-          <p> description</p>
+          <p> HTML </p>
+          <p> CSS </p>
         </div>
       </li>
       <li>
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPYwVKS5-aMVxNp7h1Q98GlR-KkMcZJ0Mdbs4zmJlHiJtr6xxnYw" width="100%" height="100%">
-          <div class="info">
-            <h3> project </h3>
-            <p> description</p>
-          </div>
-      </li>
-      <li>
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBhE-6jruhplNJrRNmFmA-eQVkNNGvU-Xlgp6yVsE2p9j4gExC" width="100%" height="100%">
+        <img src="../assets/list-vue.png" width="100%" height="100%">
         <div class="info">
-        <h3> project </h3>
-        <p> description</p>
-      </div></li>
-      <li>
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBhE-6jruhplNJrRNmFmA-eQVkNNGvU-Xlgp6yVsE2p9j4gExC" width="100%" height="100%">
-          <div class="info">
-            <h3> project </h3>
-            <p> description</p>
-          </div>
-      </li>
-      <li>
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlEVJPwNSdqhsyH3Td6Idsd8RHalVbatqivD-4bKyqPYtQ_d5y" width="100%" height="100%">
-          <div class="info">
-            <h3> project </h3>
-            <p> description</p>
-          </div>
-      </li>
-      <li>
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPYwVKS5-aMVxNp7h1Q98GlR-KkMcZJ0Mdbs4zmJlHiJtr6xxnYw" width="100%" height="100%">
-        <div class="info">
-          <h3> project </h3>
-          <p> description</p>
+         <p> HTML</p>
+         <p> CSS </p>
+         <p> JAVASCRIPT </p>
         </div>
       </li>
       <li>
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVCSQRVmXcyfcmW8bd6IBj4fTDpZAYmFIQlDibBOPbxAT0Ha2miw" width="100%" height="100%">
+        <img src="../assets/registration.png" width="100%" height="100%">
         <div class="info">
-          <h3> project </h3>
-          <p> description</p>
+         <p> HTML</p>
+         <p> CSS </p>
+         <p> JAVASCRIPT </p>
         </div>
       </li>
       <li>
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlEVJPwNSdqhsyH3Td6Idsd8RHalVbatqivD-4bKyqPYtQ_d5y" width="100%" height="100%">
+        <img src="../assets/todo.png" width="100%" height="100%">
+          <div class="info">
+          <p> HTML</p>
+          <p> CSS </p>
+          <p> JAVASCRIPT </p>
+          </div>
+      </li>
+      <li>
+          <img src="../assets/aboutme.png" width="100%" height="100%">
+          <div class="info">
+          <p> HTML</p>
+          <p> CSS </p>
+          <p> JAVASCRIPT </p>
+          </div>
+      </li>
+      <li>
+        <img src="../assets/portlandia.png" width="100%" height="100%">
         <div class="info">
-          <h3> project </h3>
-          <p> description</p>
+          <p> HTML</p>
+          <p> CSS </p>
+          <p> JAVASCRIPT </p>
         </div>
       </li>
       <li>
-        <img src="https://i.pinimg.com/236x/ef/4d/38/ef4d386f6e15552196e51b38a5c504cc--online-art-gallery-resin-art.jpg" width="100%" height="100%">
+        <img src="../assets/going-fullstack.png" width="100%" height="100%">
+        <div class="info">
+          <p> HTML</p>
+          <p> CSS </p>
+          <p> JAVASCRIPT </p>
+          <p> SQL </p>
+        </div>
+      </li>
+      <li>
+        <img src="../assets/lorem.png" width="100%" height="100%">
+        <div class="info">
+          <p> HTML</p>
+          <p> CSS </p>
+          <p> JAVASCRIPT </p>
+        </div>
+      </li>
+      <li>
+        <img src="../assets/twitch.png" width="100%" height="100%">
           <div class="info">
-            <h3> project </h3>
-            <p> description</p>
+          <p> HTML</p>
+          <p> CSS </p>
+          <p> JAVASCRIPT </p>
+          <p> SQL </p>
+          <p> API </p>
+          <p> VUE </p>
           </div>
       </li>
       <li>
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVCSQRVmXcyfcmW8bd6IBj4fTDpZAYmFIQlDibBOPbxAT0Ha2miw" width="100%" height="100%">
+        <img src="../assets/goals.png" width="100%" height="100%">
           <div class="info">
-            <h3> project </h3>
-            <p> description</p>
+            <p> HTML</p>
+            <p> CSS </p>
+            <p> JAVASCRIPT </p>
+            <p> SQL </p>
+            <p> VUE </p>
           </div>
       </li>
       <li>
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPYwVKS5-aMVxNp7h1Q98GlR-KkMcZJ0Mdbs4zmJlHiJtr6xxnYw" width="100%" height="100%">
+        <img src="../assets/search-the-web.png" width="100%" height="100%">
           <div class="info">
-            <h3> project </h3>
-            <p> description</p>
+            <p> HTML</p>
+            <p> CSS </p>
+            <p> JAVASCRIPT </p>
+            <p> SQL </p>
+            <p> API </p>
+            <p> VUE </p>
           </div>
       </li>
       <li>
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlEVJPwNSdqhsyH3Td6Idsd8RHalVbatqivD-4bKyqPYtQ_d5y" width="100%" height="100%">
+        <img src="../assets/return-of-pokedex.png" width="100%" height="100%">
           <div class="info">
-            <h3> project </h3>
-            <p> description</p>
+            <p> HTML</p>
+            <p> CSS </p>
+            <p> JAVASCRIPT </p>
           </div>
       </li>
       <li>
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVCSQRVmXcyfcmW8bd6IBj4fTDpZAYmFIQlDibBOPbxAT0Ha2miw" width="100%" height="100%">
+        <img src="../assets/calculator.png" width="100%" height="100%">
           <div class="info">
-            <h3> project </h3>
-            <p> description</p>
+            <p> HTML</p>
+            <p> CSS </p>
+            <p> JAVASCRIPT </p>
           </div>
       </li>
       <li>
@@ -104,43 +126,62 @@
 
 <style scoped lang="postcss">
 @import url('https://fonts.googleapis.com/css?family=Nanum+Myeongjo');
+@import url('https://fonts.googleapis.com/css?family=Asap');
+@import url('https://fonts.googleapis.com/css?family=Poiret+One');
 
-h1 {
+
+h3 {
   text-align: center;
-  font-family: 'Nanum Myeongjo', serif;
+  font-family: 'Sawarabi Gothic', sans-serif;
+  /* color: white; */
+  color: #A28070;
+  font-size: 1.5em;
+  display: inline-block;
 }
 ul {
   display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
-  grid-auto-rows: 200px;
+  grid-template-columns: repeat(auto-fit, minmax(400px, 1fr));
+  grid-auto-rows: 300px;
   grid-auto-flow: dense;
-  grid-column-gap: 5px;
-  grid-row-gap:5px;
+  grid-column-gap: 15px;
+  grid-row-gap:15px;
 }
 li:nth-child(2n) {
-  grid-row: span 1.5;
-  grid-column: span 1;
-}
-li:nth-child(3n) {
   grid-row: span 2;
   grid-column: span 1.5;
 }
+li:nth-child(3n) {
+  grid-row: span 1.5;
+  grid-column: span 1;
+}
 li:nth-child(4n) {
-  grid-row: span 2.5;
-  grid-column: span 2;
+  grid-row: span 1.75;
+  grid-column: span 1.75.;
+}
+li:nth-child(5n) {
+  grid-row: span 1.3;
+  grid-column: span 1;
 }
 li {
-  display: flex;
-  justify-content: center;
   align-items: center;
   background-repeat: no-repeat;
   background-size: cover;
+  background-image:#FFFEFD;
+  box-shadow: 1pt 1pt 1pt #A28070;
 } 
 li {
   position: relative;
   overflow: hidden;
 &:hover {
     img{
+        /* transform: scale(1.1); */
+        opacity: .8;
+        z-index: -1;
+        -webkit-filter: blur(10px);
+        -moz-filter: blur(10px);
+        -o-filter: blur(10px);
+        -ms-filter: blur(10px);
+        filter: blur(10px);
         filter: grayscale(0); 
         }
         .info {
@@ -150,19 +191,29 @@ li {
 }
 img {
   transition: 500ms;
+  opacity: .5;
   filter: grayscale(1);
+  position: absolute;
+  z-index: -1;
 }
 .info {
   transition: 500ms;
   position: absolute;
-  bottom: -100px;
-  height: 90px;
+  bottom: -100%;
+  height: 100%;
   width: 100%;
-  background: rgba(255, 255, 255, 0.8);
+  background:rgba(228, 211, 203, 0.618);
   text-align: center;
-  padding: 5px;
 }
-h3, p {
+p {
   margin: 0;
+  color: white;
+  /* color: #A28070; */
+  font-family: 'Poiret One', cursive;
+  text-shadow: 1pt 1pt 1pt grey;
+  font-weight: bold;
+  align-content: center;
+  margin-top: 10%;
 }
+
 </style>
